f92cf3b1b150c98902d163bfa5776b8314151292
AminMehri <aminmehri45@gmail.com>
2024-03-20 17:33:06 +0330

    Start Frontend

---
+++ frontend/src/views/PathNotFound.vue
@@ -0,0 +1,17 @@
+<template>
+
+	<div class="PageNotFound">
+		<div class="container text-center p-5">
+			<h1 id="text-404">404</h1>
+			<p >.صفحه مورد نظر شما یافت نشد</p>
+			<hr>
+		</div>
+	</div>
+
+</template>
+
+<style>
+#text-404 {
+	font-size: clamp(7rem, 40vmin, 20rem);
+}
+</style>
